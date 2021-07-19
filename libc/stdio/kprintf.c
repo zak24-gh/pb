c253fb4af09029fa41f44a1e67da3da287528693
Zak24 <87516112+zak24-gh@users.noreply.github.com>
2021-07-19 03:00:42 -0700

    Add files via upload

--- libc/stdio/kprintf.c
+++ libc/stdio/kprintf.c
@@ -3,7 +3,13 @@
 #include <stdarg.h>
 #include <limits.h>
 #include <string.h>
+#include <math.h>
 #include <kernel/mb.h>
+#include <kernel/tty.h>
+
+const char* hex_lower = "0123456789abcdef";
+const char* hex_upper = "0123456789ABCDEF";
+const char* dec = "0123456789";
 
 // Used to print characters and return false if EOF is found
 static bool print(const char* data, size_t size, multiboot_info_t* mbi)
@@ -50,7 +56,6 @@ int kprintf(multiboot_info_t* mbi, const char* restrict format, ...)
             written += amount;
             continue;
         }
-        else {}
 
         const char* fmt_start = format++;
 
@@ -63,13 +68,11 @@ int kprintf(multiboot_info_t* mbi, const char* restrict format, ...)
             {
                 // Overflow
                 return -1;
-            }
-            else {}
+            }   
 
             if (!print(&ch, sizeof(ch), mbi))
                 return -1;
-            else {}
-
+            
             written++;
         }
         else if (*format == 's')
@@ -84,11 +87,87 @@ int kprintf(multiboot_info_t* mbi, const char* restrict format, ...)
                 // Overflow
                 return -1;
             }
-            else {}
-
+            
             if (!print(str, length, mbi))
                 return -1;
 
+            written += length;
+        }
+        else if (*format == 'd')
+        {
+            format++;
+            int value = va_arg(args, int);
+
+            char buf[32] = {[31] = 0};
+            int i = 30;
+
+            bool isneg = (value < 0);
+
+            if (isneg)
+                value = -value;
+            
+            if (value)
+            {
+                for (; value && i; i--, value /= 10)
+                    buf[i] = dec[value % 10];
+            }
+            else
+            {
+                buf[i] = '0';
+                i--;
+            }
+            
+            if (isneg)
+            {
+                buf[i] = '-';
+                i--;
+            }
+            
+            char* intstr = &buf[i + 1];
+            size_t length = strlen(intstr);
+
+            if (length > max_chars)
+            {
+                // Overflow
+                return -1;
+            }
+            
+            if (!print(intstr, length, mbi))
+                return -1;
+            
+            written += length;
+        }
+        else if ((*format == 'u') || (*format == 'i'))
+        {
+            format++;
+            unsigned int value = va_arg(args, unsigned int);
+
+            char buf[32] = {[31] = 0};
+            int i = 30;
+
+            if (value)
+            {
+                for (; value && i; i--, value /= 10)
+                    buf[i] = dec[value % 10];
+            }
+            else
+            {
+                buf[i] = '0';
+                i--;
+            }
+            
+            char* intstr = &buf[i + 1];
+            size_t length = strlen(intstr);
+
+            if (length > max_chars)
+            {
+                // Overflow
+                return -1;
+            }
+            
+            if (!print(intstr, length, mbi))
+                return -1;
+            
             written += length;
         }
         else
@@ -102,12 +181,10 @@ int kprintf(multiboot_info_t* mbi, const char* restrict format, ...)
                 // Overflow
                 return -1;
             }
-            else {}
 
             if (!print(format, length, mbi))
                 return -1;
-            else {}
-
+            
             written += length;
             format += length;
         }
