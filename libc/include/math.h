28748b048c3393c21af84f6576aa1e4d746cec6f
Zak24 <87516112+zak24-gh@users.noreply.github.com>
2021-07-29 00:59:47 -0700

    Add files via upload

--- libc/include/math.h
+++ libc/include/math.h
@@ -1,15 +1,33 @@
 #ifndef _MATH_H
 #define _MATH_H
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 // Returns absolute value of x
-static inline int abs(int x)
+static inline double abs(int x)
 {
     return (x >= 0) ? x : -x;
 }
+
+double ipow(double x, int64_t y);
+
+uint64_t fact(uint8_t x);
+
+double nrt(double x, double n);
+
+double sqrt(double x);
+
+double log(double x);
+
+double ln(double x);
+
+double exp(double x);
+
+double pow(double x, double y);
 
 #ifdef __cplusplus
 }
