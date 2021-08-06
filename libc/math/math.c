4468944093b55e653595c1b9786f7f4c9ed04f21
Zak24 <87516112+zak24-gh@users.noreply.github.com>
2021-08-06 03:34:46 -0700

    Add files via upload

--- libc/math/math.c
+++ libc/math/math.c
@@ -2,6 +2,7 @@
 #include <limits.h>
 #include <math.h>
 
+// Mathematical constants of interest also defined here
 #define E (2.7182818284590452)
 #define PI (3.1415936535897932)
 
@@ -9,6 +10,7 @@ double ipow_hlpr(double x, uint64_t y)
 {
     double power = 1;
     
+    // Multiplies power by x a total of y times. If y is 0, power will be 1.
     for (int i = 0; i < y; i++)
         power *= x;
         
@@ -19,6 +21,7 @@ double ipow(double x, int64_t y)
 {
     double power = 0;
 
+    // ipow_hlpr with support for negative integer exponents, not supported by normal pow()
     if (y > 0)
         power = ipow_hlpr(x, y);
     else if (y < 0)
@@ -33,6 +36,7 @@ uint64_t fact(uint8_t x)
 {
     uint64_t _fact = 1;
     
+    // Multiples _fact by all numbers up to x and returns _fact
     for (uint8_t i = 1; i <= x; i++)
         _fact *= i;
         
@@ -43,13 +47,17 @@ double nrt(double x, double n)
 {
 	double _nrt = 0;
 	double current = 1;
-	double epsilon = 0.0000000000000001;
+	const double epsilon = 0.0000000000000001;
     double diff = INT_MAX;
     double prev = 1;
 
 	if (x < 0)
 		return -1;
 
+    /* This algorithm can produce square roots within an accuracy
+    range of a predefined constant epsilon. (As of 0.0.0.9) this
+    could be useful for some exponents under 1; however, this has
+    not been tested yet. */
 	while (diff >= epsilon)
 	{
 		current = (((n - 1) * prev) + ((double) x / ipow(prev, n - 1))) / (double) n;
@@ -63,6 +71,7 @@ double nrt(double x, double n)
 
 double sqrt(double x)
 {
+    // Nth root of x when n is 2 is square root of x
 	return nrt(x, 2);
 }
 
@@ -73,6 +82,10 @@ double log_hlpr(double x)
     double ans = 0;
     uint8_t digit = 0;
 
+    /* This algorithm can reproduce the base 10 logarithm of any
+    value greater than 1 up to 16 digits. It has already been
+    implemented in log() as a means of finding logarithms of values
+    smaller than 1. */
     for (int i = 0; i < 17; i++)
     {
         while (current > 1)
@@ -95,6 +108,7 @@ double log_hlpr(double x)
 
 double log(double x)
 {
+    // Returns error (-1) if undefined; else, the function returns logs of values greater than 0.
     if (x <= 0)
         return -1;
     else
@@ -103,6 +117,8 @@ double log(double x)
 
 double ln(double x)
 {
+    /* Log base 10 of x all divided by log base 10 of E will yield log
+    base E of x. Guaranteed to work for all values greater than 0. */
     return log(x) / log(E);
 }
 
@@ -110,7 +126,7 @@ double exp(double x)
 {
     double power = 1;
     int crnt_exp = 0;
-    double epsilon = 0.0000000000000001;
+    const double epsilon = 0.0000000000000001;
 
     while (x > ln(power))
     {
@@ -118,6 +134,10 @@ double exp(double x)
         crnt_exp++;
     }
 
+    /* This algorithm zeroes in on exp(x) using ln(x) within an accuracy
+    range of a predefined constant epsilon. Specifically, this algorithm
+    uses powers of 2 to develop the closest posible answer within this
+    range, subtracting or adding them based on the value of power. */
     while (abs(x - ln(power)) > epsilon)
     {
         crnt_exp--;
@@ -134,5 +154,7 @@ double exp(double x)
 
 double pow(double x, double y)
 {
+    /* Returns x^y in manner using exp() and ln() functions. Currently only
+    guaranteed to work for x- and y-values greater than 1. */
     return exp(y * ln(x));
 }
